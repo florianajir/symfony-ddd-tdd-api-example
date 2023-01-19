ad5a5d0bff8e6e7874563ef97f76faada537eba6
Florian Ajir <florianajir@gmail.com>
2023-01-19 20:59:04 +0400

    Add profiler pack and swagger ui for debugging purposes

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
